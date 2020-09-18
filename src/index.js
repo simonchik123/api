b49ffc2900704470d7f7e01819b6bfd75188502d
Байков Максим Федорович <Baykov_MF@apk-invest.com>
2020-09-18 14:37:16 +0300

    p_26

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
 // index.js
 // This is the main entry point of our application
+
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 4000;
+const {ApolloServer, gql} = require('apollo-server-express');
+
+
+
+let notes = [
+        { id: '1', content: 'Первая заметка', author: 'Байков Максим' },
+        { id: '2', content: 'Вторая статья', author: 'Вася Пупкин' },
+        { id: '3', content: 'Опа статья, гаршочек, не вари!', author: 'Джон Уик' }
+    ];
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql `
+type Note {
+    id: ID!
+    content: String!
+    author: String!
+    }
+
+type Query {
+        hello: String!
+        notes: [Note!]!
+        }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!',
+        notes: () => notes
+        }
+    };
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.get('/', (req, res) => res.send('Hello World!!! HI'));
+app.listen({port}, () => console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`));
