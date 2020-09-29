e7a95bb68e4258f88fa9adf0cd26f7415505abae
Omen <admin@mail.com>
2020-09-29 22:55:53 +0300

    users_part_2

--- src/resolvers/mutation.js
+++ src/resolvers/mutation.js
@@ -1,3 +1,15 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const {
+    AuthenticationError,
+    ForbiddenError
+} = require('apollo-server-express');
+require('dotenv').config();
+const gravatar = require('../util/gravatar');
+
+
+
+
 module.exports = {
     newNote: async (parent, args,{Models}) => {
         return await Models.Note.create({
@@ -25,5 +37,52 @@ module.exports = {
         } catch (err) {
             return false;
         }
-    }
+    },
+
+    signUp: async (parent, { username, email, password }, { Models }) => {
+        // normalize email address
+        email = email.trim().toLowerCase();
+        // hash the password
+        const hashed = await bcrypt.hash(password, 10);
+        // create the gravatar url
+        const avatar = gravatar(email);
+        try {
+            const user = await Models.User.create({
+                username,
+                email,
+                avatar,
+                password: hashed
+            });
+            // create and return the json web token
+            return jwt.sign({
+                id: user._id
+            }, process.env.JWT_SECRET);
+        } catch (err) {
+            console.log(err);
+            // if there's a problem creating the account, throw an error
+            throw new Error('Error creating account');
+            }
+        },
+        signIn: async (parent, { username, email, password }, { Models }) => {
+            if (email) {
+                // normalize email address
+                email = email.trim().toLowerCase();
+            }
+            const user = await Models.User.findOne({
+                $or: [{email}, {username}]
+            });
+            // if no user is found, throw an authentication error
+            if (!user) {
+                throw new AuthenticationError('Error signing in');
+            }
+            // if the passwords don't match, throw an authentication error
+            const valid = await bcrypt.compare(password, user.password);
+            if (!valid) {
+                throw new AuthenticationError('Error signing in');
+            }
+            // create and return the json web token
+            return jwt.sign({
+                id: user._id
+            }, process.env.JWT_SECRET);
+            }
 };
